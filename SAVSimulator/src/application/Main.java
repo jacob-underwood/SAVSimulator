f24cf92a3c7165f1d1085aadacf11e9bfe46bbca
jacob-underwood <jacobu002@gmail.com>
2022-11-06 22:39:42 -0700

    Refactored city set up.
    Created new CityDisplay class to handle manipulation of graphical
    output.

--- SAVSimulator/src/application/Main.java
+++ SAVSimulator/src/application/Main.java
@@ -12,25 +12,7 @@ import javafx.scene.shape.StrokeType;
 
 public class Main extends Application {
 
-//	final double screenXBorderPixels = 2.0;
-//	final double screenYBorderPixels = 70.0;
-	final int screenSize = 700;
-	// How many intersections in one side. Square to get total number.
-	final int gridSize = 15;
-	// Radius of circles representing intersections.
-	final int intersectRadius = 8;
-	// Total extra boundary added in a certain direction. Halve to get cushion added
-	// on a singular side.
-	final int extraBoundary = 20;
-	
-	// Radius difference from intersection to ring.
-	final int ringDistance = 2;
-	// Stroke size is added on top of the radius.
-	final int ringStrokeSize = 2;
-	
-	// Distance from ring to center of person.
-	final int personDistance = 4;
-	final int personRadius = 2;
+
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -40,46 +22,14 @@ public class Main extends Application {
 			// Get screen sizes.
 //			double screenX = Screen.getPrimary().getBounds().getMaxX() - screenXBorderPixels;
 //			double screenY = Screen.getPrimary().getBounds().getMaxY() - screenYBorderPixels;
-
+			
+			int screenSize = 700;
+			
 			Scene scene = new Scene(root, screenSize, screenSize, Color.BLACK);
 
 			// Generate intersection layout. Current total intersections: 225.
-			Group intersections = new Group();
-			Group busMarkers = new Group();
-			Group people = new Group();
-			for (int i = 0; i < gridSize; i++) {
-				for (int j = 0; j < gridSize; j++) {
-					Circle circle = new Circle(intersectRadius, Color.web("gray"));
-					circle.setCenterX((i) * (screenSize - extraBoundary) / gridSize
-							+ (screenSize) / ((gridSize + gridSize * 2) / 2));
-					circle.setCenterY((j) * (screenSize - extraBoundary) / gridSize
-							+ (screenSize) / ((gridSize + gridSize * 2) / 2));
-					intersections.getChildren().add(circle);
-					
-					Circle ring = new Circle(intersectRadius + ringDistance, Color.web("transparent"));
-					ring.setCenterX(circle.getCenterX());
-					ring.setCenterY(circle.getCenterY());
-					ring.setStrokeType(StrokeType.OUTSIDE);
-					ring.setStroke(Color.web("red"));
-					ring.setStrokeWidth(ringStrokeSize);
-					ring.setVisible(false);
-					busMarkers.getChildren().add(ring);
-					
-					double personRingXRadius = intersectRadius + ringDistance + ringStrokeSize + personDistance;
-					double personRingYRadius = intersectRadius + ringDistance + ringStrokeSize + personDistance;
-					for (int k = 0; k < 8; k++) {
-						Circle person = new Circle(personRadius, Color.web("blue"));
-						person.setCenterX(circle.getCenterX() + personRingXRadius * Math.cos(k * Math.PI / 4));
-						person.setCenterY(circle.getCenterY() + personRingYRadius * Math.sin(k * Math.PI / 4));
-						person.setVisible(false);
-						people.getChildren().add(person);
-					}
-				}
-			}
-
-			root.getChildren().add(intersections);
-			root.getChildren().add(busMarkers);
-			root.getChildren().add(people);
+			CityDisplay cityDisplay = new CityDisplay(root);
+			cityDisplay.setupCity(screenSize, 15, 8, 20, 2, 2, 4, 2);
 
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
