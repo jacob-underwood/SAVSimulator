3cde383609d24db8a05abc137f0db962ba4adbdc
jacob-underwood <jacobu002@gmail.com>
2022-11-24 20:48:54 -0700

    Implement underlying classes Intersection and Person.
    Removed totalWaitTime from person because that can be calculated from
    timeInBus + timeAtStop.

--- SAVSimulator/src/application/Intersection.java
+++ SAVSimulator/src/application/Intersection.java
@@ -3,26 +3,35 @@ package application;
 public class Intersection {
 
 	private int x, y;
-	
+
 	public Intersection(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	/**
 	 * @param other Intersection to compare this to.
-	 * @return Number of steps to get to other assuming a bus would go directly there.
+	 * @return Number of steps to get to other assuming a bus would go directly
+	 *         there.
 	 */
 	public int distanceFrom(Intersection other) {
-		return -1;
+		/*
+		 * Distance is measured by how much a bus must go horizontally and vertically,
+		 * not by diagonals. In a grid city, a bus can only go north, south, east, and
+		 * west. The fastest route is the one with the least turns, so it should just go
+		 * all the way horizontally, then all the way vertically.
+		 */
+		int distance = (other.getX() - x) + (other.getY() - y);
+
+		return Math.abs(distance);
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
+
 }
--- SAVSimulator/src/application/Person.java
+++ SAVSimulator/src/application/Person.java
@@ -5,25 +5,22 @@ package application;
  */
 public class Person {
 
-	// Steps waited in total since creation of person.
-	private int totalWaitTime;
 	// Steps waited in bus.
 	private int timeInBus;
 	// Steps waited at stop.
 	private int timeAtStop;
 	private boolean inBus;
-	
+
 	// Current location of person, whether they are in bus or not.
 	private Intersection location;
-	
+
 	public Person(Intersection location) {
 		this.location = location;
-		totalWaitTime = 0;
 		timeInBus = 0;
 		timeAtStop = 0;
 		inBus = false;
 	}
-	
+
 	/**
 	 * Changes location of a Person.
 	 * 
@@ -31,15 +28,75 @@ public class Person {
 	 * @return true if successful.
 	 */
 	public boolean move(Intersection newLocation) {
-		return false;
+		// TODO: Check how many people are already there. If there are more than can be
+		// displayed, do not allow a person to move there? Or, make a way to display
+		// that there are more.
+		
+		location = newLocation;
+
+		return true;
 	}
-	
+
 	/**
 	 * Increments step counter instance variables depending on person's state.
 	 */
 	public void incrementTime() {
-		
+		if (inBus) {
+			timeInBus++;
+		} else {
+			timeAtStop++;
+		}
 	}
-	
-	
+
+	/**
+	 * @return the location
+	 */
+	public Intersection getLocation() {
+		return location;
+	}
+
+	/**
+	 * @return the inBus
+	 */
+	public boolean isInBus() {
+		return inBus;
+	}
+
+	/**
+	 * @param inBus the inBus to set
+	 */
+	public void setInBus(boolean inBus) {
+		this.inBus = inBus;
+	}
+
+	/**
+	 * Calculated the total wait time by adding the time waiting inside and outside a bus.
+	 * 
+	 * @return the total wait time
+	 */
+	public int getTotalWaitTime() {
+		return timeInBus + timeAtStop;
+	}
+
+	/**
+	 * @return the timeInBus
+	 */
+	public int getTimeInBus() {
+		return timeInBus;
+	}
+
+	/**
+	 * @return the timeAtStop
+	 */
+	public int getTimeAtStop() {
+		return timeAtStop;
+	}
+
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(Intersection location) {
+		this.location = location;
+	}
+
 }
